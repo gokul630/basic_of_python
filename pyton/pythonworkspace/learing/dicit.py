cf6aa76e40ad56b9d91c78c7fd15bf5eeb1e1774
gokul630 <gokulrajgltnk0075@gmail.com>
2026-06-02 23:18:14 +0530

    Initial commit

---
+++ pyton/pythonworkspace/learing/dicit.py
@@ -0,0 +1,12 @@
+detile={'name':'Gokul','hight':7.4,'age':25}
+print(detile['name'])
+print(detile.get('name','not found'))
+print(detile.get('gokul','not found'))## not found will displayed when key is not found
+
+
+ex1={'contact','place','age'}
+lis1=[9972481014,'bangalore',45]
+dis1=dict(zip(ex1,lis1))##dict is the inbuild fuction of dicinary and zip is inbulid function to combine
+print(dis1)
+
+
---
+++ pyton/pythonworkspace/learing/list.py
@@ -0,0 +1,9 @@
+detile=('gokul',25,7.6)
+print(type(detile))
+name,age,hight=detile
+print(f'my name is {name}\n'f'my age is {age}\n'f'my hight is {hight}')
+
+detile2=(detile,[12,35,45])
+detile2[1][2] = 65
+print(detile2)
+
