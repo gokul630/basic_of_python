f15374425c9ef75cfc58b792f89ca3f8fe1be64d
gokul630 <gokulrajgltnk0075@gmail.com>
2026-06-03 22:02:55 +0530

    03-06

---
+++ pyton/pythonworkspace/learing/datatype.py
@@ -0,0 +1,18 @@
+dateofborn = 22
+hight = 4.5
+comp = 2+3j
+areyouaboy = True
+r =range(0,10)
+
+k=int(hight)
+
+
+print(type(dateofborn))
+print(type(hight))
+print(type(comp))
+print(type(k))
+print(type(areyouaboy))
+print(type(r))
+
+
+
